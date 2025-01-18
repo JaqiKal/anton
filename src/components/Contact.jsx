bc1d633f74c863f8656377e101cbf2a3911db27c
JaqiKal <jaqika@gmail.com>
2025-01-18 13:07:23 +0100

    feat: Add a template for Contact
    Unsure if it is going to be used

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+/**
+ * Contact.jsx
+ *
+ * Author: JaqiKal
+ * Date: January 2025
+ *
+ */
+
+import React from "react";
+
+function Contact() {
+  return <div>Contact</div>;
+}
+
+export default Contact;
