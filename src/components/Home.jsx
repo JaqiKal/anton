5dc58638782280758c78b5f7c01e979e90d59119
JaqiKal <jaqika@gmail.com>
2025-01-19 12:19:14 +0100

    fix: Ensure VideoCard renders properly in Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,6 +21,7 @@
 
 import React from "react";
 import VideoCard from "../components/VideoCard";
+import image3DThumbnail from "../assets/images/3DThumbnail_v001.webp";
 
 const Home = () => {
   const video = {
@@ -28,17 +29,18 @@ const Home = () => {
     title: "Anton Skogsberg - Short Loop Animation Showreel",
     borderColor: "#0a0a0a",
     description: "", // No description needed for Home
+    imageUrl: image3DThumbnail,
   };
 
   return (
-    <div className="flex items-center justify-center min-h-[80vh]">
-      <div className="video-wrapper mt-10 sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/3 shadow-lg rounded overflow-hidden">
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="video-wrapper w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/3 shadow-lg rounded overflow-hidden">
         <VideoCard
           video={video}
-          autoplay
-          loop
-          muted
-          showDescription={false}
+          autoplay={true} // Autoplay
+          loop={true} // Loop infinitely
+          muted={true} // Mute the video
+          showDescription={false} // Hide the description
         />
       </div>
     </div>
